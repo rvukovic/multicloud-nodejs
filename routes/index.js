6d1fa7c7017b8492495b39b00795c7336ef355f4
Robert Vukovic <rvukovic@gmail.com>
2017-03-09 15:35:19 +0100

    Azure PoC for Blob and Queue added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+var azure = require('azure-storage');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+ //----------------------------------------------------
+    var blobSvc = azure.createBlobService();
+
+    blobSvc.createBlockBlobFromLocalFile('images-in', '.gitignore',
+        '/Users/robert/Work/Tests/Azure/Arch9/multicloud-nodejs/.gitignore',
+        function (error, result, response) {
+            if (!error) {
+                console.log('file uploaded');
+            } else {
+                console.log('ERROR: blob upload: ' + error);
+            }
+        });
+    //----------------------------------------------------
+
+    var queueSvc = azure.createQueueService();
+    var now = new Date().toISOString();
+    queueSvc.createMessage('a9-queue-items', "Message " + now, function (error, result, response) {
+        if (!error) {
+            console.log('Message created');
+        } else {
+            console.log('ERROR: Queue message:' + error);
+        }
+    });
+    //----------------------------------------------------
+
+
+
   res.render('index', { title: 'Express' });
 });
 
