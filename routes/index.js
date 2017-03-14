d5a78401d575a6edf7905dbd86bd4b1b01292a24
Robert Vukovic <rvukovic@gmail.com>
2017-03-14 13:02:38 +0100

    Azure table storage implementation

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var azure = require('azure-storage');
 router.get('/', function (req, res, next) {
 
     res.render('index', {
-        title: 'Multi cloud PoC - v0.06',
+        title: 'Multi cloud PoC - v0.07',
         cloudService: process.env['CLOUD_SERVICE']
     });
 });
