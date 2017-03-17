c10f0f7cc9068966ff2717bf958731c1395f6120
Robert Vukovic <rvukovic@gmail.com>
2017-03-17 12:48:17 +0100

    FIXED callback flow Azure function was not resending proper JSON

--- _functions/azure/QueueTrigger/index.js
+++ _functions/azure/QueueTrigger/index.js
@@ -6,11 +6,13 @@ module.exports = function (context, myQueueItem) {
     context.log('Request: ' + JSON.stringify(myQueueItem));
 
     request.post({
+        url: myQueueItem.callbackUrl,
+        json: true,
+        body: myQueueItem,
         headers: {
             'content-type': 'application/json'
-        },
-        url: myQueueItem.callbackUrl,
-        form: myQueueItem
+        }
+
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             context.log('Response: ' + JSON.stringify(body))
